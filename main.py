47a9b7bd823990f133fd5d18cdfb45958cb064b2
BulzyKrown Big Boss <darkmagiclost@gmail.com>
2022-02-25 10:17:14 -0300

    pruebas

--- main.py
+++ main.py
@@ -1,57 +1,53 @@
+import importlib
+import os
+
 from quart import Quart, render_template, jsonify
-from render import achievement, amiajoke, bad, calling, captcha, didyoumean, drake, facts, filter, floor, fml, jokeoverhead, opinion, pornhub, salty, scroll, shame, ship, what
+
+
+api_keys = os.getenv('API_KEYS', 'uwu').split(',')
+
+no_auth_paths = ['/', '/filter']
 
 app = Quart(__name__)
 
+
+# Get all endpoints
+for file in os.listdir('render'):
+    if file.endswith('.py'):
+        lib = importlib.import_module(f"render.{file[:-3]}")
+        app.register_blueprint(getattr(lib, 'blueprint'))
+
+
 @app.route('/')
-async def index():
+def home():
     return jsonify({
         "endpoints": [
-            "/achievement?text=text[&icon=int]",
-            "/challenge?text=text[&icon=int]",
-            "/amiajoke?image=url",
-            "/bad?image=url",
-            "/calling?text=text",
-            "/captcha?text=text",
-            "/didyoumean?top=text&bottom=text",
-            "/drake?top=text&bottom=text",
-            "/facts?text=text",
-            "/floor?image=url&text=text",
-            "/fml",
-            "/jokeoverhead?image=url",
-            "/trash?face=url&trash=url",
-            "/pornhub?text=text&text2=text",
-            "/salty?image=url",
-            "/scroll?text=text",
-            "/saa   hame?image=url",
-            "/ship?user=url&user2=url",
-            "/what?image=url"
+            "GET /achievement?text=text[&icon=int]",
+            "GET /challenge?text=text[&icon=int]",
+            "GET /amiajoke?image=url",
+            "GET /bad?image=url",
+            "GET /calling?text=text",
+            "GET /captcha?text=text",
+            "GET /didyoumean?top=text&bottom=text",
+            "GET /drake?top=text&bottom=text",
+            "GET /facts?text=text",
+            "GET /filter",
+            "GET /floor?image=url&text=text",
+            "GET /fml",
+            "GET /jokeoverhead?image=url",
+            "GET /trash?face=url&trash=url",
+            "GET /pornhub?text=text&text2=text",
+            "GET /salty?image=url",
+            "GET /scroll?text=text",
+            "GET /shame?image=url",
+            "GET /ship?user=url&user2=url",
+            "GET /what?image=url"
         ],
-        "credits": [
-            "AlexFlipnote, argo0n, bentettmar"
-        ]
+        "original": {
+            "created_by": "AlexFlipnote",
+            "repo": "https://github.com/AlexFlipnote/alex_api_archive",
+        }
     })
-
-app.register_blueprint(achievement.blueprint)
-app.register_blueprint(amiajoke.blueprint)
-app.register_blueprint(bad.blueprint)
-app.register_blueprint(calling.blueprint)
-app.register_blueprint(captcha.blueprint)
-app.register_blueprint(didyoumean.blueprint)
-app.register_blueprint(drake.blueprint)
-app.register_blueprint(facts.blueprint)
-app.register_blueprint(filter.blueprint)
-app.register_blueprint(floor.blueprint)
-app.register_blueprint(fml.blueprint)
-app.register_blueprint(jokeoverhead.blueprint)
-app.register_blueprint(opinion.blueprint)
-app.register_blueprint(pornhub.blueprint)
-app.register_blueprint(salty.blueprint)
-app.register_blueprint(scroll.blueprint)
-app.register_blueprint(shame.blueprint)
-app.register_blueprint(ship.blueprint)
-app.register_blueprint(what.blueprint)
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
